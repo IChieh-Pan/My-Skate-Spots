ecf25abaab55cee9edfbd9c9663a32ec748f17e8
IChieh-Pan <ellenpan7174@gmail.com>
2021-03-23 21:48:51 +0100

    wrappedMap added

--- src/App.js
+++ src/App.js
@@ -1,67 +1,23 @@
-import React, { Component } from "react";
-import { Map, GoogleApiWrapper, InfoWindow, Marker } from "google-maps-react";
+import React from "react";
+import { GoogleMap, withScriptjs, withGoogleMap } from "react-google-maps";
 
-const mapStyles = {
-  width: "100%",
-  height: "100%",
-};
-
-export class MapContainer extends Component {
-  state = {
-    showingInfoWindow: false, // Hides or shows the InfoWindow
-    activeMarker: {}, // Shows the active marker upon click
-    selectedPlace: {}, // Shows the InfoWindow to the selected place upon a marker
-  };
-
-  onMarkerClick = (props, marker, e) =>
-    this.setState({
-      selectedPlace: props,
-      activeMarker: marker,
-      showingInfoWindow: true,
-    });
-
-  onClose = (props) => {
-    if (this.state.showingInfoWindow) {
-      this.setState({
-        showingInfoWindow: false,
-        activeMarker: null,
-      });
-    }
-  };
-
-  render() {
-    return (
-      <Map
-        google={this.props.google}
-        zoom={14}
-        style={mapStyles}
-        initialCenter={{
-          lat: -1.2884,
-          lng: 36.8233,
-        }}
-      >
-        <Marker
-          onClick={this.onMarkerClick}
-          name={"Kenyatta International Convention Centre"}
-        />
-        <InfoWindow
-          marker={this.state.activeMarker}
-          visible={this.state.showingInfoWindow}
-          onClose={this.onClose}
-        >
-          <div>
-            <h4>{this.state.selectedPlace.name}</h4>
-          </div>
-        </InfoWindow>
-      </Map>
-    );
-  }
+function Map() {
+  return (
+    <GoogleMap
+      defaultZoom={10}
+      defaultCenter={{ lat: 52.48206, lng: 13.42484 }}
+    />
+  );
 }
 
-/* export default GoogleApiWrapper({
-  apiKey: `${process.env.REACT_APP_API_KEY}`,
-})(MapContainer); */
+const WrappedMap = withScriptjs(withGoogleMap(Map));
 
-export default GoogleApiWrapper((props) => ({
-  apiKey: `props.${process.env.GOOGLE_MAPS_KEY}`,
-}))(MapContainer);
+
+
+function App() {
+  return <div>
+    <WrappedMap googleMapURL =  />
+  </div>;
+}
+
+export default App;
